09acc74a4aca944bd6ab64852efc21979ff75710
Ard2238 <abhishekrdas28@gmail.com>
2020-10-07 01:31:16 +0530

    Refactored Code to validate last name

--- src/main/java/com/cg/UserReg.java
+++ src/main/java/com/cg/UserReg.java
@@ -7,6 +7,7 @@ public class UserReg {
     private static Scanner sc = new Scanner(System.in);
 
     /* UC1 -- Validate First Name */
+    /* UC2 -- Validate Last Name */
     private boolean validateName(String firstName) {
         return Pattern.matches("[A-Z][a-z]{2,}", firstName);
     }
@@ -15,9 +16,12 @@ public class UserReg {
         System.out.println("Welcome to the User Registration Program");
         System.out.println("Enter the first name: ");
         String firstName = sc.next();
+        System.out.println("Enter the last name: ");
+        String lastName = sc.next();
 
         UserReg userReg = new UserReg();
         System.out.println(userReg.validateName(firstName));
+        System.out.println(userReg.validateName(lastName));
 
     }
 }
